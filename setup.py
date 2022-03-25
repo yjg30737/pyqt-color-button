0b2dc36e887dd5035408d6dd1e433d36b565f624
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-25 13:23:38 +0900

    Rename project, directory (color_button -> pyqt_color_button)

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='color-button',
+    name='pyqt-color-button',
     version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='circle-shaped color button',
-    url='https://github.com/yjg30737/color-button.git',
+    url='https://github.com/yjg30737/pyqt-color-button.git',
     install_requires=[
         'PyQt5>=5.8'
     ]
